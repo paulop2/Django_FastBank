d785dcb56df8541859f929525a565d5847476868
Laura Pigosso <127753547+LauraPigosso@users.noreply.github.com>
2023-11-22 10:52:25 -0300

    Add files via upload

--- user/permissions.py
+++ user/permissions.py
@@ -1,6 +1,36 @@
-from rest_framework import permissions
+"""
+Serializers for the user API.
+"""
+from django.contrib.auth import get_user_model
 
-class IsCreationOrIsAthenticated(permissions.BasePermission):
-    # def has_object_permission(self, request, view):
-    #     return bool(request.user and request.user.is_authenticated)
-        
+from rest_framework import serializers
+
+from django.utils.translation import gettext as _
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'first_name', 'last_name', 'cpf', 'created_at', 'url_imagem']
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 6},
+            'is_active': {'read_only': True},
+            'created_at': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        """Create and return a new with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def updated(slef, instance, validated_data):
+        """Update and return a user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
