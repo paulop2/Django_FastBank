9de881bb35ba4da385a2fd87d24bbb5c773f0f4d
54793421857 <laurapigosso@gmail.com>
2023-11-23 16:59:20 -0300

    Models

--- core/models.py
+++ core/models.py
@@ -1,51 +1,68 @@
-
+"""
+Models de toda a aplicaçãp
+"""
 import os
 import uuid
-from django.conf import settings 
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager,
+    AbstractBaseUser,
+    BaseUserManager,
     PermissionsMixin
 )
 from django.utils import timezone
+from django.conf import settings
+
 
 def user_image_field(instance, filename):
+    """Generate file path for new user image."""
     ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'user', filename)
 
-class UserManager( BaseUserManager ):
+
+class Conta(models.Model):
+    """Conta para cada um dos cliente(usuários)"""
+    agencia = models.CharField(max_length=4, unique=True)
+    numero = models.CharField(max_length=8)
+    saldo = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING
+    )
+
+
+class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email, password = None, **extra_fiels):
-        """Create, save and return a new user,"""
+    def create_user(self, email, password=None, **extra_fiels):
+        """Create, save and return a new user."""
         if not email:
-            raise ValueError("Voce precisa de um email")
-        
+            raise ValueError("Voce precissa de um email")
+
         user = self.model(email=self.normalize_email(email), **extra_fiels)
         user.set_password(password)
         user.save()
 
         return user
-    
+
     def create_superuser(self, email, password):
-        """Create, save and return a new super user,"""
+        """Create, save and return a new super user."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
-    
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in system"""
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, null=False)
     last_name = models.CharField(max_length=255, null=False)
-    cpf = models.CharField(max_length=11, null=False, unique=True)
-    url_image = models.ImageField(null = True, upload_to = user_image_field)
+    cpf = models.CharField(max_length=11, unique=True, null=False)
+    url_imagem = models.ImageField(null=True, upload_to=user_image_field)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
@@ -55,4 +72,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
     def __str__(self) -> str:
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
