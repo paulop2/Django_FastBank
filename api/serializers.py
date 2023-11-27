724867979476d212dea17f7010a874dc83c2d1f7
LauraPigosso <laurapigosso@gmail.com>
2023-11-27 00:45:28 -0300

    terminei aulas

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from core.models import Conta
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conta
-        fields = ['agencia', 'numero']
+        fields = ['id', 'agencia', 'numero']
         read_only_fields = ['numero']
 
 
@@ -13,3 +13,16 @@ class AccountDetailSerializer(AccountSerializer):
     class Meta(AccountSerializer.Meta):
         fields = AccountSerializer.Meta.fields + ['id', 'saldo', 'created_at']
         read_only_fields = AccountSerializer.Meta.read_only_fields + ['id', 'saldo', 'created_at']
+
+
+class DepositoSerializer(serializers.Serializer):
+    value = serializers.DecimalField(max_digits=5, decimal_places=2)
+    
+    class Meta:
+        fields = ['value']
+
+class SaqueSerializer(serializers.Serializer):
+    value = serializers.DecimalField(max_digits=5, decimal_places=2)
+    
+    class Meta:
+        fields = ['value']
